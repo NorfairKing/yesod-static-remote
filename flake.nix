31c66610453058a7be79a0517017dc1e51475f67
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-10-02 04:11:26 +0200

    Nix flake

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "yesod-static-remote";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
+    nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
+    nixpkgs-21_05.url = "github:NixOS/nixpkgs?ref=nixos-21.05";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-22_05
+    , nixpkgs-21_11
+    , nixpkgs-21_05
+    , pre-commit-hooks
+    }:
+    let
+      system = "x86_64-linux";
+      pkgsFor = nixpkgs: import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.${system}
+        ];
+      };
+      pkgs = pkgsFor nixpkgs;
+    in
+    {
+      overlays.${system} = import ./nix/overlay.nix;
+      packages.${system}.default = pkgs.haskellPackages.yesod-static-remote;
+      checks.${system} =
+        let
+          backwardCompatibilityCheckFor = nixpkgs:
+            let pkgs' = pkgsFor nixpkgs;
+            in pkgs'.haskellPackages.yesod-static-remote;
+          allNixpkgs = {
+            inherit
+              nixpkgs-22_05
+              nixpkgs-21_11
+              nixpkgs-21_05;
+          };
+          backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
+        in
+        backwardCompatibilityChecks // {
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              hlint.enable = true;
+              hpack.enable = true;
+              ormolu.enable = true;
+              nixpkgs-fmt.enable = true;
+              nixpkgs-fmt.excludes = [ "default.nix" ];
+              cabal2nix.enable = true;
+            };
+          };
+        };
+      devShells.${system}.default = pkgs.haskellPackages.shellFor {
+        name = "yesod-static-remote-shell";
+        packages = (p: with p; [
+          yesod-static-remote
+        ]);
+        withHoogle = true;
+        doBenchmark = true;
+        buildInputs = with pkgs; [
+          niv
+          zlib
+          cabal-install
+        ] ++ (with pre-commit-hooks;
+          [
+            hlint
+            hpack
+            nixpkgs-fmt
+            ormolu
+            cabal2nix
+          ]);
+        shellHook = self.checks.${system}.pre-commit.shellHook;
+      };
+    };
+}
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,17 +1,12 @@
-final: previous:
+final: prev:
 with final.haskell.lib;
-
 {
-  haskellPackages =
-    previous.haskellPackages.override (
-      old:
-      {
-        overrides =
-          final.lib.composeExtensions (old.overrides or (_: _: { })) (
-            self: super: {
-              yesod-static-remote = disableLibraryProfiling (dontCheck (failOnAllWarnings (self.callCabal2nix "yesod-static-remote" (final.gitignoreSource ../.) { })));
-            }
-          );
-      }
-    );
+  haskellPackages = prev.haskellPackages.override (old: {
+    overrides =
+      final.lib.composeExtensions (old.overrides or (_: _: { })) (
+        self: super: {
+          yesod-static-remote = buildStrictly (self.callPackage ../default.nix { });
+        }
+      );
+  });
 }
