289a35a2729f187e65658b1a7c1bfab485e6eb18
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-05-26 12:46:39 +0200

    Drop really old backward compatibility

--- flake.nix
+++ flake.nix
@@ -6,11 +6,6 @@
     nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     nixpkgs-23_11.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
-    nixpkgs-23_05.url = "github:NixOS/nixpkgs?ref=nixos-23.05";
-    nixpkgs-22_11.url = "github:NixOS/nixpkgs?ref=nixos-22.11";
-    nixpkgs-22_05.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
-    nixpkgs-21_11.url = "github:NixOS/nixpkgs?ref=nixos-21.11";
-    nixpkgs-21_05.url = "github:NixOS/nixpkgs?ref=nixos-21.05";
   };
 
   outputs =
@@ -19,11 +14,6 @@
     , nixpkgs-24_11
     , nixpkgs-24_05
     , nixpkgs-23_11
-    , nixpkgs-23_05
-    , nixpkgs-22_11
-    , nixpkgs-22_05
-    , nixpkgs-21_11
-    , nixpkgs-21_05
     , pre-commit-hooks
     }:
     let
@@ -48,12 +38,7 @@
             inherit
               nixpkgs-24_11
               nixpkgs-24_05
-              nixpkgs-23_11
-              nixpkgs-23_05
-              nixpkgs-22_11
-              nixpkgs-22_05
-              nixpkgs-21_11
-              nixpkgs-21_05;
+              nixpkgs-23_11;
           };
           backwardCompatibilityChecks = pkgs.lib.mapAttrs (_: nixpkgs: backwardCompatibilityCheckFor nixpkgs) allNixpkgs;
         in
