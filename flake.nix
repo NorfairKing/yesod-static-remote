3bdb8f6ff63e203486e3f696c32d5ac5274f98f0
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-12-03 10:58:41 +0100

    Upgrade to nixos-25.11 and lts-24.16

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   description = "yesod-static-remote";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nixpkgs-25_05.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
     nixpkgs-24_11.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     nixpkgs-24_05.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     nixpkgs-23_11.url = "github:NixOS/nixpkgs?ref=nixos-23.11";
@@ -11,6 +12,7 @@
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-25_05
     , nixpkgs-24_11
     , nixpkgs-24_05
     , nixpkgs-23_11
@@ -36,6 +38,7 @@
             in pkgs'.haskellPackages.yesod-static-remote;
           allNixpkgs = {
             inherit
+              nixpkgs-25_05
               nixpkgs-24_11
               nixpkgs-24_05
               nixpkgs-23_11;
